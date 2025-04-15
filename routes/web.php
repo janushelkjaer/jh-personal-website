12e5503d1ca61c50c7e7997efcd43934eef93df2
Janus Helkjær <janus.helkjaer@gmail.com>
2025-04-15 17:02:32 +0200

    block comps mockups

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
+Route::get('/{any?}', [PageController::class, 'show'])->where('any', '.*')->name('pages.show');
 
 require __DIR__.'/auth.php';
