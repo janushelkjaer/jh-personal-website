fe22ffb20eae8341f428cceeefe29f382bfd633c
Janus Helkjær <janus.helkjaer@gmail.com>
2025-04-24 23:33:04 +0200

    logo fix rounded

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex size-8 items-center justify-center">
-    <x-app-logo-icon class="size-8" />
+    <x-app-logo-icon class="size-8 rounded-full" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold">Janus Helkjær</span>
