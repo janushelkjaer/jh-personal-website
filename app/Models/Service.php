2d5ce04f44442b7a1a61d9c846ee886da569a34a
Janus Helkjær <janus.helkjaer@gmail.com>
2025-05-15 23:07:29 +0200

    service model scope for order column

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\Builder;
 
 class Service extends BaseModel
 {
     use HasTranslations;
     protected $translatable = ['title', 'slug', 'content', 'seo', 'meta', 'description', 'short_description'];
+
+    public function scopeBySortOrder(Builder $query): Builder
+    {
+        return $query->orderBy('order', 'asc');
+    }
 }
